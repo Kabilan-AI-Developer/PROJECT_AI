728444d2b48288ed2dddefabd72048313caa9888
santhosh2718 <santhoshstatistics@gmail.com>
2024-12-31 15:28:08 +0530

    Update femod.py

--- femod.py
+++ femod.py
@@ -1,13 +1,36 @@
 import cv2
+import os
 from mtcnn import MTCNN
 from mtcnn.utils.images import load_image
 class FaceCap:
-    
-
+    def train(self):
+        pass
+    def pred(self):
+        pass
     def __init__(self):
-        #Creating a Video Capture fe
+        self.detector = MTCNN(device="CPU:0")
+        #os.chdir('img/')
+    def train(self):
+        os.chdir('img/')
+        self.files=[]
+        self.m=os.listdir()
+        for self.file in self.m:
+            if self.file.endswith(".jpg"):
+                self.files.append(self.file)
+        for self.file in self.files:
+            self.a=cv2.imread(self.file)
+            print(self.file)
+            self.location=self.detector.detect_faces(self.a)
+            if len(self.location)>0:
+                    for self.face in self.location:
+                        self.score = self.face["confidence"]
+                        if self.score >= 0.90:
+                            self.x1,self.y1,self.width,self.height=self.face['box']
+                            self.b=self.a[int(self.y1):int(self.y1+self.height), int(self.x1):int(self.x1+self.width)]
+                            cv2.imwrite("ext/extract_"+self.file,self.b)
+    def pred(self):
+        self.Out_img=0
         self.cap = cv2.VideoCapture(0)
-        #Creating a MTCNN detector object
         self.detector = MTCNN(device="CPU:0")
         self.count=0
         while True:
@@ -30,7 +53,7 @@ class FaceCap:
                 self.key=cv2.waitKey(1)
         
                 if self.key==ord('s'):
-                    cv2.imwrite("captured_image.jpg",self.frame[int(self.y1):int(self.y1+self.height), int(self.x1):int(self.x1+self.width)]) #The cropped output image
+                    cv2.imwrite("captured.jpg",self.frame[int(self.y1):int(self.y1+self.height), int(self.x1):int(self.x1+self.width)]) #The cropped output image
                     break
         
                 if self.key==ord('q'):
@@ -39,3 +62,4 @@ class FaceCap:
                 break
         self.cap.release()
         cv2.destroyAllWindows()
+        return 
